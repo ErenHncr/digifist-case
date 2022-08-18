ee78f4377960ee7c492e41a9e8c75060d5e2cc7d
erenhncr <erenhncr@gmail.com>
2022-08-18 20:39:22 +0300

    remove gh-pages scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 import Layout from './components/partials/Layout';
 import App from './App';
@@ -10,6 +16,14 @@ const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(
   <Layout>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
   </Layout>,
 );
